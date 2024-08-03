873e05d9c0b41d59911bae390c20a211f7cc83a0
Khoi Nguyen <nguyen.pk@geekup.vn>
2024-08-03 12:13:24 +0700

    ignore

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from models.item import Item
 from models.transaction import Transaction
 from models.node import Node
+from typing import  Set
 
 milk = Item("Milk", 0.4)
 fruit = Item("Fruit", 0.9)
@@ -8,23 +9,18 @@ video = Item("Video", 0.6)
 
 tom_transaction = Transaction({milk: 0.4, fruit: 1.0, video: 0.3})
 lucy_transaction = Transaction({milk: 1.0, fruit: 0.8})
+item_set: Set[Item] = {milk, fruit}  
+sup = 0.0  
+e_sup = 0.0  
+p_sup = 0.0  
+lb = 5.0  
+ub = 0.0 
+parent = None  
+children = [] 
+avg_weight = 0.65
 
-print(video)
-
-item_set = {milk, fruit, video}
-
-node = Node(
-    item_set=item_set,
-    sup=2.0,
-    e_sup=1.5,
-    p_sup=0.8,
-    lb=0.3,
-    ub=0.9,
-    parent=None,
-    children=[],
-    avg_weight=0.75
-)
+node = Node[Item, Transaction](item_set, sup, e_sup, p_sup, lb, ub, parent, children, avg_weight)
 
 
 
-
+print(node)
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,30 @@
+from typing import TypeVar, Generic, Set, List
+
+from models.item import Item
+from models.transaction import Transaction
+
+E = TypeVar('E', bound='Item')
+T = TypeVar('T', bound='Transaction')
+
 def formatDouble(number, decimal_places=2) -> float:
     return round(number, decimal_places)
 
+def is_list_contained(self, larger: List[E], small: Set[E]) -> bool:
+    return set(larger).issuperset(small)
+
+def not_existed_in_set(self, Ck: List[Set[E]], candidate: Set[E]) -> bool:
+    for set_item in Ck:
+        if set_item.issuperset(candidate) and len(set_item) == len(candidate):
+            return False
+    return True
+
+def get_difference_of_two_lists(self, first_list: Set[E], second_list: Set[E]) -> Set[E]:
+    difference_set = set(first_list)
+    difference_set.difference_update(second_list)
+    return difference_set
+
+def get_distinct_list_from_w_PFIs(self, W_PFIs: List[Set[E]]) -> Set[E]:
+    set_item = set()
+    for item_list in W_PFIs:
+        set_item.update(item_list)
+    return set_item
